5b1b99d3c1e464c32fd71c2f71df0172f6b3f4ab
John Figueiredo <john@johnfigueiredo.com>
2013-10-07 22:20:14 -0700

    Set up production and development database

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,20 @@
 class Photo < ActiveRecord::Base
 	attr_accessible :photo, :name
 
-  has_attached_file :photo, :styles => { :small => "150x150>" },
-                  :url  => "/assets/products/:id/:style/:basename.:extension",
-                  :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+	if Rails.env.development?
+	  has_attached_file :photo, :styles => { :small => "150x150>" },
+	                  :url  => "/assets/products/:id/:style/:basename.:extension",
+	                  :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+	elsif Rails.env.production?
+  	has_attached_file :photo, :styles => { :thumb => '40x40#', :medium => '150x200>', :large => '300x300>'},
+                    :convert_options => { :thumb => "-quality 92", :medium => "-quality 92", :large => "-quality 92"  },
+                    :storage => :s3,
+								    :s3_credentials => {
+								      :bucket => ENV['AWS_BUCKET'],
+								      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+								      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+								    }
+	end
 
 	validates_attachment_presence :photo
 	validates_attachment_size :photo, :less_than => 5.megabytes
